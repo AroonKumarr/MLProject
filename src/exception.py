25488663732e4c08d0176463ac8fd75b31be599e
Aroon Kumar <aroonk644@gmail.com>
2025-05-05 19:35:54 +0500

    added another page for exception, which will print exceptions

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,8 @@
 import sys
 
+# add these two libraries
+import logging
+from logger import logging
 
 # The funciton is about error message details:
 
@@ -17,9 +20,14 @@ class CustomException(Exception):
         super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_detail=error_detail)
 
-
+        # and also added this:
+        logging.error(self.error_message)
     def __str__(self):
         return self.error_message
     
-    
+if __name__ == "__main__":    
 
+    try:
+        10 / 0
+    except Exception as e:
+        raise CustomException(e, sys)
